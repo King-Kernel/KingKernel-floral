a6254d0b11d8aa9ab045f8ebac3dc53f5e9bd32e
Hemant Kumar <hemantk@codeaurora.org>
2019-10-03 17:35:04 -0700

    mhi: core: Call vfree instead of kfree
    mhi channel struct pointer is allocated using vzalloc. This pointer
    is freed using kfree, which causes page fault. Fix this issue by
    calling vfree.
    
    Change-Id: Ibead85231c5cfd3f2e3f9776fdbbd4a9e4163311
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/bus/mhi/core/mhi_init.c
+++ drivers/bus/mhi/core/mhi_init.c
@@ -1086,7 +1086,7 @@ static int of_parse_dt(struct mhi_controller *mhi_cntrl,
 	return 0;
 
 error_ev_cfg:
-	kfree(mhi_cntrl->mhi_chan);
+	vfree(mhi_cntrl->mhi_chan);
 
 	return ret;
 }
@@ -1202,7 +1202,7 @@ error_alloc_dev:
 	kfree(mhi_cntrl->mhi_cmd);
 
 error_alloc_cmd:
-	kfree(mhi_cntrl->mhi_chan);
+	vfree(mhi_cntrl->mhi_chan);
 	kfree(mhi_cntrl->mhi_event);
 
 	return ret;
@@ -1215,7 +1215,7 @@ void mhi_unregister_mhi_controller(struct mhi_controller *mhi_cntrl)
 
 	kfree(mhi_cntrl->mhi_cmd);
 	kfree(mhi_cntrl->mhi_event);
-	kfree(mhi_cntrl->mhi_chan);
+	vfree(mhi_cntrl->mhi_chan);
 	kfree(mhi_cntrl->mhi_tsync);
 
 	device_del(&mhi_dev->dev);
